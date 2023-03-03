cb2397f8e107d6d50c9fb62c6b09019ce0ba8849
Balint Meszaros <meszaros.balint65@gmail.com>
2023-03-03 17:09:34 +0100

    Add: Genre class

---
+++ src/main/java/com/codecool/eventorganizer/model/Genre.java
@@ -0,0 +1,32 @@
+package com.codecool.eventorganizer.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+@Entity
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String type;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
