641791ea125943cbfb220439fc8763ccaea96a8f
Balint Meszaros <meszaros.balint65@gmail.com>
2023-03-05 13:57:52 +0100

    Add: GlobalControllerExceptionHandler

---
+++ src/main/java/com/codecool/eventorganizer/controller/GlobalControllerExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.codecool.eventorganizer.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({SQLException.class, NoSuchElementException.class, IllegalArgumentException.class,
+        IllegalStateException.class})
+    @ResponseBody
+    public String handleBadRequest(Throwable exception) {
+        return exception.getMessage();
+    }
+
+}
