73687238d84c626c3a1efc27684ec6ed9b511b86
Balint Meszaros <meszaros.balint65@gmail.com>
2023-03-05 12:09:33 +0100

    Add: custom exceptions for refund and book

---
+++ src/main/java/com/codecool/eventorganizer/exception/CustomExceptions.java
@@ -0,0 +1,17 @@
+package com.codecool.eventorganizer.exception;
+
+// TODO ask if plural naming is conventional or not
+public class CustomExceptions {
+    // TODO ask if state is good here or not
+    public static class EventCanNotBeRefunded extends IllegalStateException {
+        public EventCanNotBeRefunded(String message) {
+            super(message);
+        }
+    }
+
+    public static class EventCanNotBeBooked extends IllegalStateException {
+        public EventCanNotBeBooked(String message) {
+            super(message);
+        }
+    }
+}
