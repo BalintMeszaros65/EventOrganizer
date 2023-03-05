efd7ca3055f1158d7d1025e4730b288cff1751bd
Balint Meszaros <meszaros.balint65@gmail.com>
2023-03-05 14:41:55 +0100

    Add: getAppUserByEmail

--- src/main/java/com/codecool/eventorganizer/repository/AppUserRepository.java
+++ src/main/java/com/codecool/eventorganizer/repository/AppUserRepository.java
@@ -4,9 +4,12 @@ import com.codecool.eventorganizer.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
+
     boolean existsByEmail(String email);
+    Optional<AppUser> getAppUserByEmail(String email);
 }
