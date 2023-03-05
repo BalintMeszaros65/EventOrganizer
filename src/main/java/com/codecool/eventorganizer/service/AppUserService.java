082457027abd1de4bbd12798e8510df9aa3283c2
Balint Meszaros <meszaros.balint65@gmail.com>
2023-03-05 14:10:47 +0100

    Add: registerUser

--- src/main/java/com/codecool/eventorganizer/service/AppUserService.java
+++ src/main/java/com/codecool/eventorganizer/service/AppUserService.java
@@ -1,8 +1,11 @@
 package com.codecool.eventorganizer.service;
 
+import com.codecool.eventorganizer.exception.CustomExceptions;
 import com.codecool.eventorganizer.model.AppUser;
 import com.codecool.eventorganizer.repository.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -34,6 +37,24 @@ public class AppUserService {
 
     public void deleteUser(UUID id) {
         appUserRepository.deleteById(id);
+    }
+
+    public ResponseEntity<String> registerUser(AppUser appUser) {
+        if (appUser.getEmail() == null || appUser.getPassword() == null || appUser.getFirstName() == null
+                || appUser.getLastName() == null) {
+            throw new CustomExceptions.MissingAttributeException("Missing one or more attribute(s) in AppUser\n");
+        } else {
+            if (IsEmailAlreadyInUse(appUser.getEmail())) {
+                throw new CustomExceptions.EmailAlreadyUsedException("Email is already registered.\n");
+            } else {
+                appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
+                appUser.setRoles(List.of("ROLE_USER"));
+                saveAndUpdateUser(appUser);
+            }
+        }
+        // TODO token creation after security
+        String token = "placeholder";
+        return ResponseEntity.status(HttpStatus.CREATED).body(token);
     }
 
     private boolean IsEmailAlreadyInUse(String email) {
