0f90716bed1d6fdc962e70180d3be3d162849adc
Balint Meszaros <meszaros.balint65@gmail.com>
2023-03-07 09:31:47 +0100

    Add: CustomUserDetailsService

---
+++ src/main/java/com/codecool/eventorganizer/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.codecool.eventorganizer.service;
+
+import com.codecool.eventorganizer.model.AppUser;
+import com.codecool.eventorganizer.model.CustomUserDetails;
+import com.codecool.eventorganizer.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    AppUserRepository appUserRepository;
+
+    @Autowired
+    public CustomUserDetailsService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<AppUser> optionalAppUser = appUserRepository.findByEmail(email);
+        if (optionalAppUser.isPresent()) {
+            AppUser savedAppUser = optionalAppUser.get();
+            return new CustomUserDetails(savedAppUser);
+        } else {
+            throw new UsernameNotFoundException(String.format("No user found by given email: %s", email));
+        }
+    }
+}
