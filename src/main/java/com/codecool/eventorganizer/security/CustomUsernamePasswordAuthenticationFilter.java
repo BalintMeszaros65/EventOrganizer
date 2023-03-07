de7af61d1da5ca9f36b4b61732d2bdc4b0ec241e
Balint Meszaros <meszaros.balint65@gmail.com>
2023-03-07 12:40:50 +0100

    Add: CustomUsernamePasswordAuthenticationFilter

---
+++ src/main/java/com/codecool/eventorganizer/security/CustomUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,38 @@
+package com.codecool.eventorganizer.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    public CustomUsernamePasswordAuthenticationFilter(@Lazy AuthenticationManager authenticationManager, JwtUtil jwtUtil, CustomUserDetailsService customUserDetailsService) {
+        super(authenticationManager);
+        this.jwtUtil = jwtUtil;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        String email = authResult.getName();
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+        String token = jwtUtil.generateToken(userDetails);
+        response.getWriter().println(token);
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
