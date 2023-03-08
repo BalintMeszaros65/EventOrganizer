6d4698ef2340c80d568588b7371472778d7683e8
Balint Meszaros <meszaros.balint65@gmail.com>
2023-03-08 17:06:42 +0100

    Refactor: structure and methods to remove code duplication

--- src/main/java/com/codecool/eventorganizer/service/GenreService.java
+++ src/main/java/com/codecool/eventorganizer/service/GenreService.java
@@ -22,6 +22,7 @@ public class GenreService {
         this.genreRepository = genreRepository;
     }
 
+    // basic CRUD operations
     public void saveAndUpdateGenre(Genre genre) {
         genreRepository.save(genre);
     }
@@ -30,36 +31,39 @@ public class GenreService {
         return genreRepository.findAllDistinctType();
     }
 
-    public ResponseEntity<String> deleteGenre(Genre genre) {
-        UUID id = genre.getId();
-        if (!genreRepository.existsById(id)) {
-            throw new NoSuchElementException("Genre not found by given id.");
-        }
-        genreRepository.deleteById(id);
-        return ResponseEntity.status(HttpStatus.OK).body("Genre deleted successfully.");
-    }
-
-    public ResponseEntity<String> createGenre(Genre genre) {
+    // helper methods
+    private static void checkIfRequiredDataExists(Genre genre) {
         if (genre.getName() == null || genre.getType() == null || "".equals(genre.getName())
                 || "".equals(genre.getType())) {
             throw new CustomExceptions.MissingAttributeException("Missing one or more attribute(s) in genre.");
         }
+    }
+
+    private void checkIfGenreExists(UUID id) {
+        if (!genreRepository.existsById(id)) {
+            throw new NoSuchElementException("Genre not found by given id.");
+        }
+    }
+
+    // logic
+    public ResponseEntity<String> createGenre(Genre genre) {
+        checkIfRequiredDataExists(genre);
         saveAndUpdateGenre(genre);
         return ResponseEntity.status(HttpStatus.CREATED).body("Genre created successfully.");
     }
 
-
     public ResponseEntity<String> updateGenre(Genre genre) {
-        UUID id = genre.getId();
-        if (id == null || genre.getName() == null || genre.getType() == null || "".equals(genre.getName())
-                || "".equals(genre.getType())) {
-            throw new CustomExceptions.MissingAttributeException("Missing one or more attribute(s) in genre.");
-        }
-        if (!genreRepository.existsById(id)) {
-            throw new NoSuchElementException("Genre not found by given id.");
-        }
+        checkIfRequiredDataExists(genre);
+        checkIfGenreExists(genre.getId());
         saveAndUpdateGenre(genre);
         return ResponseEntity.status(HttpStatus.OK).body("Genre updated successfully.");
+    }
+
+    public ResponseEntity<String> deleteGenre(Genre genre) {
+        UUID id = genre.getId();
+        checkIfGenreExists(id);
+        genreRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Genre deleted successfully.");
     }
 
     public List<Genre> getAllGenreByType(String type) {
