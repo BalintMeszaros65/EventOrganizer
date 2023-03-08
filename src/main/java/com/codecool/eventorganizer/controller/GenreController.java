767278300a725edf370c51d6c37561f7100aaa65
Balint Meszaros <meszaros.balint65@gmail.com>
2023-03-08 13:16:52 +0100

    Add: createGenre

---
+++ src/main/java/com/codecool/eventorganizer/controller/GenreController.java
@@ -0,0 +1,25 @@
+package com.codecool.eventorganizer.controller;
+
+import com.codecool.eventorganizer.model.Genre;
+import com.codecool.eventorganizer.service.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenreController {
+    GenreService genreService;
+
+    @Autowired
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    @PostMapping("/api/genre/create")
+    public ResponseEntity<String> createGenre(@RequestBody Genre genre) {
+        return genreService.createGenre(genre);
+    }
+
+}
--- src/main/java/com/codecool/eventorganizer/service/GenreService.java
+++ src/main/java/com/codecool/eventorganizer/service/GenreService.java
@@ -1,8 +1,11 @@
 package com.codecool.eventorganizer.service;
 
+import com.codecool.eventorganizer.exception.CustomExceptions;
 import com.codecool.eventorganizer.model.Genre;
 import com.codecool.eventorganizer.repository.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
@@ -34,4 +37,14 @@ public class GenreService {
     public void deleteGenre(UUID id) {
         genreRepository.deleteById(id);
     }
+
+    public ResponseEntity<String> createGenre(Genre genre) {
+        if (genre.getName() == null || genre.getType() == null || "".equals(genre.getName())
+                || "".equals(genre.getType())) {
+            throw new CustomExceptions.MissingAttributeException("Missing one or more attribute(s) in genre.");
+        }
+        saveAndUpdateGenre(genre);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Genre created successfully.");
+    }
+
 }
