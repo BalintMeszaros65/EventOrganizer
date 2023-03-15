3bc28dd03a610f226e8f744c1d7407a7434c8f3c
Balint Meszaros <meszaros.balint65@gmail.com>
2023-03-15 15:16:06 +0100

    Add: saveBookedEvent

--- src/main/java/com/codecool/eventorganizer/service/BookedEventService.java
+++ src/main/java/com/codecool/eventorganizer/service/BookedEventService.java
@@ -1,37 +1,76 @@
 package com.codecool.eventorganizer.service;
 
+import com.codecool.eventorganizer.exception.CustomExceptions;
+import com.codecool.eventorganizer.model.AppUser;
 import com.codecool.eventorganizer.model.BookedEvent;
+import com.codecool.eventorganizer.model.Event;
+import com.codecool.eventorganizer.model.Genre;
 import com.codecool.eventorganizer.repository.BookedEventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.UUID;
+import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class BookedEventService {
     private final BookedEventRepository bookedEventRepository;
+    private final EventService eventService;
+    private final AppUserService appUserService;
+    private final GenreService genreService;
 
     @Autowired
-    public BookedEventService(BookedEventRepository bookedEventRepository) {
+    public BookedEventService(BookedEventRepository bookedEventRepository, EventService eventService, AppUserService appUserService, GenreService genreService) {
         this.bookedEventRepository = bookedEventRepository;
+        this.eventService = eventService;
+        this.appUserService = appUserService;
+        this.genreService = genreService;
     }
 
-    public void saveAndUpdateBookedEvent(BookedEvent bookedEvent) {
-        bookedEventRepository.save(bookedEvent);
+    // basic CRUD operations
+
+    public List<BookedEvent> getBookedEventsByEvent(Event event) {
+        return bookedEventRepository.findAllByEvent(event);
     }
 
-    public BookedEvent getBookedEvent(UUID id) {
-        Optional<BookedEvent> optionalBookedEvent = bookedEventRepository.findById(id);
-        if (optionalBookedEvent.isPresent()) {
-            return optionalBookedEvent.get();
-        } else {
-            throw new NoSuchElementException("Booked event not found by given id.");
+    // helper methods
+
+    private void checkIfRequiredDataExists(BookedEvent bookedEvent) {
+        Event event = bookedEvent.getEvent();
+        AppUser appUser = bookedEvent.getAppUser();
+        Genre genre = bookedEvent.getGenre();
+        int ticketsBooked = bookedEvent.getTicketsCount();
+        BigDecimal amountPayed = bookedEvent.getAmountPayed();
+        if (event == null || amountPayed == null || BigDecimal.ZERO.equals(amountPayed) || ticketsBooked <= 0
+             || bookedEvent.getDateOfBooking() == null || appUser == null || genre == null ||
+                bookedEvent.isRefunded()) {
+            throw new CustomExceptions.MissingAttributeException("Missing one or more attribute(s) in booked event.");
+        }
+        if (!event.canBeBooked(ticketsBooked)) {
+            throw new CustomExceptions.EventCanNotBeBookedException("Event can not be booked.");
+        }
+        Event savedEvent = eventService.getEvent(event.getId());
+        if (!event.equals(savedEvent)) {
+            throw new IllegalArgumentException("Event's data does not match with the one in database.");
+        }
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        AppUser savedAppUser = appUserService.getUserByEmail(email);
+        if (!appUser.equals(savedAppUser)) {
+            throw new IllegalArgumentException("Current user data does not match with the one in booked event.");
+        }
+        Genre savedGenre = genreService.getGenreById(genre.getId());
+        if (!genre.equals(savedGenre)) {
+            throw new IllegalArgumentException("Genre's data does not match with the one in database.");
         }
     }
 
-    public void deleteBookedEvent(UUID id) {
-        bookedEventRepository.deleteById(id);
+    // logic
+    public BookedEvent saveBookedEvent(BookedEvent bookedEvent) {
+        checkIfRequiredDataExists(bookedEvent);
+        if (bookedEvent.getId() != null) {
+            throw new CustomExceptions.IdCanNotExistWhenCreatingEntityException();
+        }
+        return bookedEventRepository.save(bookedEvent);
     }
 }
