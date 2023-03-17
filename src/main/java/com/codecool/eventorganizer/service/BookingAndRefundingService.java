0a297f53672471c82ecf3b79e69484febd79573c
Balint Meszaros <meszaros.balint65@gmail.com>
2023-03-17 17:31:55 +0100

    Add: bookEvent

---
+++ src/main/java/com/codecool/eventorganizer/service/BookingAndRefundingService.java
@@ -0,0 +1,41 @@
+package com.codecool.eventorganizer.service;
+
+import com.codecool.eventorganizer.model.AppUser;
+import com.codecool.eventorganizer.model.BookedEvent;
+import com.codecool.eventorganizer.model.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class BookingAndRefundingService {
+    private final AppUserService appUserService;
+    private final BookedEventService bookedEventService;
+    private final EventService eventService;
+
+    @Autowired
+    public BookingAndRefundingService(AppUserService appUserService, BookedEventService bookedEventService, EventService eventService) {
+        this.appUserService = appUserService;
+        this.bookedEventService = bookedEventService;
+        this.eventService = eventService;
+    }
+
+    // helper methods
+
+    private AppUser getCurrentUser() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return appUserService.getUserByEmail(email);
+    }
+
+    // logic
+
+    public void bookEvent(Event event, int ticketCount) {
+        BigDecimal amountToBePayed = event.currentPriceOfTickets(ticketCount);
+        // TODO payment later?
+        eventService.bookTickets(event, ticketCount);
+        BookedEvent savedBookedEvent = bookedEventService.saveBookedEvent(new BookedEvent(event, amountToBePayed, ticketCount));
+        appUserService.addBookedEventToCurrentUser(savedBookedEvent);
+    }
+}
