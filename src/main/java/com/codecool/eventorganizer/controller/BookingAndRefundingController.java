7441d35d60c0042ce6447178830f42b223c101bb
Balint Meszaros <meszaros.balint65@gmail.com>
2023-03-17 17:46:34 +0100

    Add: bookEvent

---
+++ src/main/java/com/codecool/eventorganizer/controller/BookingAndRefundingController.java
@@ -0,0 +1,25 @@
+package com.codecool.eventorganizer.controller;
+
+import com.codecool.eventorganizer.service.BookingAndRefundingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+public class BookingAndRefundingController {
+    private final BookingAndRefundingService bookingAndRefundingService;
+
+    @Autowired
+    public BookingAndRefundingController(BookingAndRefundingService bookingAndRefundingService) {
+        this.bookingAndRefundingService = bookingAndRefundingService;
+    }
+
+    @PostMapping("/api/book/{event_id}/{ticket_count}")
+    public ResponseEntity<String> bookEvent(@PathVariable("event_id") UUID eventId, @PathVariable("ticket_count") int ticketCount) {
+        bookingAndRefundingService.bookEvent(eventId, ticketCount);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Event booked successfully.");
+    }
+}
