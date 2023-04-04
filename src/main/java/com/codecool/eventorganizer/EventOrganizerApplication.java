65544f39374f6b938e9264607d0eb94c7ec324f8
Balint Meszaros <meszaros.balint65@gmail.com>
2023-04-04 13:51:31 +0200

    Add: Bearer and Basic Authentication SecuritySchemes

--- src/main/java/com/codecool/eventorganizer/EventOrganizerApplication.java
+++ src/main/java/com/codecool/eventorganizer/EventOrganizerApplication.java
@@ -1,9 +1,31 @@
 package com.codecool.eventorganizer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(
+		name = "Bearer Authentication",
+		type = SecuritySchemeType.HTTP,
+		bearerFormat = "JWT",
+		scheme = "bearer"
+)
+@SecurityScheme(
+		name = "Basic Authentication",
+		type = SecuritySchemeType.HTTP,
+		scheme = "basic"
+)
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Event Organizer API",
+				version = "1.0",
+				description = "Event Organizer Information"
+		)
+)
 public class EventOrganizerApplication {
 
 	public static void main(String[] args) {
