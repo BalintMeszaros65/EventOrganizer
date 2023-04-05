785bfca7f28cf495a58580367d8d16887fe83d3a
Balint Meszaros <meszaros.balint65@gmail.com>
2023-04-05 18:14:11 +0200

    Refactor: indentation

--- src/main/java/com/codecool/eventorganizer/EventOrganizerApplication.java
+++ src/main/java/com/codecool/eventorganizer/EventOrganizerApplication.java
@@ -11,32 +11,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @SecurityScheme(
-		name = "Bearer Authentication",
-		type = SecuritySchemeType.HTTP,
-		bearerFormat = "JWT",
-		scheme = "bearer"
+	name = "Bearer Authentication",
+	type = SecuritySchemeType.HTTP,
+	bearerFormat = "JWT",
+	scheme = "bearer"
 )
 @SecurityScheme(
-		name = "Basic Authentication",
-		type = SecuritySchemeType.HTTP,
-		scheme = "basic"
+	name = "Basic Authentication",
+	type = SecuritySchemeType.HTTP,
+	scheme = "basic"
 )
 @OpenAPIDefinition(
-		info = @Info(
-				title = "Event Organizer API",
-				version = "1.0",
-				// TODO change the placeholders
-				description = "Event Organizer placeholder description",
-				termsOfService = "placeholder terms",
-				contact = @Contact(
-						name = "Bálint Mészáros",
-						email = "balint.meszaros65@gmail.com"
-				),
-				license = @License(
-						name = "placeholder licence",
-						url = "placeholder url"
-				)
+	info = @Info(
+		title = "Event Organizer API",
+		version = "1.0",
+		// TODO change the placeholders
+		description = "Event Organizer placeholder description",
+		termsOfService = "placeholder terms",
+		contact = @Contact(
+			name = "Bálint Mészáros",
+			email = "balint.meszaros65@gmail.com"
+		),
+		license = @License(
+			name = "Placeholder licence",
+			url = "placeholder url"
 		)
+	)
 )
 public class EventOrganizerApplication {
 
