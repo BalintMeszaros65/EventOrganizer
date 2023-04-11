5403d1f3de17a3aacffc400998fcfb6c5a1901f2
Balint Meszaros <meszaros.balint65@gmail.com>
2023-04-11 11:59:05 +0200

    Add: TODO to fix basic authentication

--- src/main/java/com/codecool/eventorganizer/security/CustomUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/codecool/eventorganizer/security/CustomUsernamePasswordAuthenticationFilter.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+// TODO fix!!
 @Component
 public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final JwtUtil jwtUtil;
