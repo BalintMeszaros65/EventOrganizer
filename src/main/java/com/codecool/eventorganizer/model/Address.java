366f94319bcde5e0e27665ae90af6edd6fa6430e
Balint Meszaros <meszaros.balint65@gmail.com>
2023-04-12 14:24:19 +0200

    Add: Address

---
+++ src/main/java/com/codecool/eventorganizer/model/Address.java
@@ -0,0 +1,47 @@
+package com.codecool.eventorganizer.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @NotNull
+    @ManyToOne
+    private City city;
+    @NotNull
+    private String zipCode;
+    @NotNull
+    private String street;
+    @NotNull
+    private String house;
+    private String googleMapsReference;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Address address)) return false;
+        return Objects.equals(id, address.id) && Objects.equals(city, address.city)
+                && Objects.equals(zipCode, address.zipCode)
+                && Objects.equals(street, address.street) && Objects.equals(house, address.house)
+                && Objects.equals(googleMapsReference, address.googleMapsReference);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, city, zipCode, street, house, googleMapsReference);
+    }
+}
