3827a0ca9aa4156db5e72080c7131bf7013203e8
Balint Meszaros <meszaros.balint65@gmail.com>
2023-04-12 14:23:09 +0200

    Add: findAllByCountry

--- src/main/java/com/codecool/eventorganizer/repository/CityRepository.java
+++ src/main/java/com/codecool/eventorganizer/repository/CityRepository.java
@@ -1,11 +1,14 @@
 package com.codecool.eventorganizer.repository;
 
 import com.codecool.eventorganizer.model.City;
+import com.codecool.eventorganizer.model.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, UUID> {
+    List<City> findAllByCountry(Country country);
 }
