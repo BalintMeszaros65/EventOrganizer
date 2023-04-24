8b062a88ed0e55d68f4c1f5f637f81bdf909a3cf
Balint Meszaros <meszaros.balint65@gmail.com>
2023-04-24 11:45:36 +0200

    Add: findByName

--- src/main/java/com/codecool/eventorganizer/repository/CountryRepository.java
+++ src/main/java/com/codecool/eventorganizer/repository/CountryRepository.java
@@ -4,8 +4,10 @@ import com.codecool.eventorganizer.model.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, UUID> {
+    Optional<Country> findByName(String name);
 }
