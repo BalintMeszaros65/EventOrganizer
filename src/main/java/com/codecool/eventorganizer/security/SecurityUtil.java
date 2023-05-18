a8c37063a311924b2cb875943bac162b136c9168
Balint Meszaros <meszaros.balint65@gmail.com>
2023-05-18 12:47:50 +0200

    Add: RequestRejectedHandler with TODO

--- src/main/java/com/codecool/eventorganizer/security/SecurityUtil.java
+++ src/main/java/com/codecool/eventorganizer/security/SecurityUtil.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.firewall.HttpStatusRequestRejectedHandler;
+import org.springframework.security.web.firewall.RequestRejectedHandler;
 
 @Order(1)
 @Configuration
@@ -11,6 +13,12 @@ public class SecurityUtil {
     @Bean
     public BCryptPasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RequestRejectedHandler requestRejectedHandler() {
+        // TODO ask why is it working (tomcat html) and not working (still 500 with blank) at the same time
+        return new HttpStatusRequestRejectedHandler();
     }
 }
 
