90173f4b9e335b16ca37955b9c29f0f9292f1022
Balint Meszaros <meszaros.balint65@gmail.com>
2023-06-01 13:09:21 +0200

    Add: RecommendationType

---
+++ src/main/java/com/codecool/eventorganizer/model/RecommendationType.java
@@ -0,0 +1,12 @@
+package com.codecool.eventorganizer.model;
+
+public enum RecommendationType {
+    TICKET_PRICE,
+    TOP,
+    PERFORMANCE,
+    ORGANIZER,
+    GENRE,
+    VENUE,
+    CITY,
+    COUNTRY
+}
