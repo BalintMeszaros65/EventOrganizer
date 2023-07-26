128531a157ce154ec41a7f3456e5211c6a2fe8a9
Balint Meszaros <meszaros.balint65@gmail.com>
2023-07-26 11:51:05 +0200

    Add: email sending with attachment

--- src/main/java/com/codecool/eventorganizer/service/EmailService.java
+++ src/main/java/com/codecool/eventorganizer/service/EmailService.java
@@ -1,9 +1,14 @@
 package com.codecool.eventorganizer.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class EmailService {
@@ -16,6 +21,19 @@ public class EmailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
+        mailSender.send(message);
+    }
+
+    public void sendEmailWithAttachment(
+            String to, String subject, String text, String pathToAttachment, String attachmentName) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        File file = new File(pathToAttachment);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text);
+        helper.addAttachment(attachmentName, file);
         mailSender.send(message);
     }
 }
